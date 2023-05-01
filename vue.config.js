979e72e6c9434ecc125aa55e55bf21cd56599e1c
Nikhil Naik <46309727+nikhilsurfingaus@users.noreply.github.com>
2023-05-01 16:38:07 +1000

    rollback

--- vue.config.js
+++ vue.config.js
@@ -25,6 +25,9 @@ module.exports = {
                 target: 'https://api.jdoodle.com',
                 changeOrigin: true,
                 secure: false,
+                pathRewrite: {
+                    '^/api': '',
+                },
             },
         },
     },
